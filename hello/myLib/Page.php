59b70ad4e316d87651e7a798b831babf0dc1fb0f
sonofmath <sonofmath3.14@hotmail.com>
2018-08-31 11:04:10 -0600

    Added format for an html file

---
+++ hello/myLib/Page.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace myLib;
+
+require_once "Generator.php";
+
+class Page extends Generator {
+    public function doctype() {
+        echo "<!DOCTYPE html>\n";
+    }
+
+    public function page() {
+        $this->doctype();
+        $this->html();
+    }
+
+    public function html() {
+        echo "<html>\n";
+        $this->head();
+        $this->body();
+        echo "</html>\n";
+    }
+
+    public function head() {
+        echo "<head>\n";
+        echo "<meta charset="UTF-8">\n";
+        $this->title();
+        echo "</head>\n";
+    }
+
+    public function title() {
+        echo "Web 3 php practice";
+    }
+
+    public function body() {
+        echo "<body>\n";
+        $this->content();
+        echo "</body>\n";
+    }
+
+    public function content() {
+        echo "<b>Hello World</b>\nSweet my php framework is working!\n";
+    }
+
+    public function generate() {
+        $this->page();
+    }
+}
