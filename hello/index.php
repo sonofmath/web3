78a32a3873a44a18fe8dd3204ba22488c99f0c86
sonofmath <sonofmath3.14@hotmail.com>
2018-08-31 11:03:06 -0600

    Simplified my index.php file

--- hello/index.php
+++ hello/index.php
@@ -1,45 +1,17 @@
 <?php
 
-include "myLib/Generator.php";
-$name = "JR";
-$names = array("alice", "bob", "cindy");
+require_once "myLib/Page.php";
 
-
-function sayHi(&$who) {
-    echo "<br>Hi $who!\n";
-    $who = "nobody";
-}
-
-
-
-class Page extends myLib\Generator {
-    public function header() {
-        echo "<html><title>No Title</title>";
+class ThisPage extends myLib\Page {
+    public function title() { 
+        echo "Home Page";
     }
-    public function footer() {
-        echo "</html>";
-    }
-    public function body() {
-        echo "<body>this is the body</body>";
-    }
-    function generate() {
-        $this->header();
-        $this->body();
-        $this->footer();
+
+    public function content() {
+        echo "Main Content";
     }
 }
 
-$page = new Page();
+$page = new ThisPage();
 $page->generate();
 ?>
-
-<html>
-    Hi, <?php echo $name ?>!
-</br>
-    Hi, 
-    <?php 
-    foreach ($names as $name) {
-        echo "<br>$name\n";
-    }
-    ?>
-</html>
